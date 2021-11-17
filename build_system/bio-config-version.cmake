39643c52b2865ad29d13619bf94b65f950d71bbc
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2021-11-17 10:51:23 +0100

    [infra] remove release candidate from versioning in camke.

--- build_system/bio-config-version.cmake
+++ build_system/bio-config-version.cmake
@@ -39,30 +39,6 @@ else()
     endif()
 endif()
 
-# extract release candidate
-file(STRINGS "${BIO_INCLUDE_DIR}/bio/version.hpp" BIO_RELEASE_CANDIDATE_HPP REGEX "#define BIO_RELEASE_CANDIDATE ")
-string(REGEX REPLACE "#define BIO_RELEASE_CANDIDATE " "" BIO_RELEASE_CANDIDATE_VERSION "${BIO_RELEASE_CANDIDATE_HPP}")
-
-# As of writing this (cmake 3.20):
-# cmake does not allow to set a version containing a suffix via `project(... VERSION 3.0.3-rc.1)`.
-# Version comparisons like VERSION_LESS, VERSION_GREATER do support comparing versions with a suffix (they just drop
-# it), see https://cmake.org/cmake/help/latest/command/if.html#version-comparisons.
-#
-# If https://gitlab.kitware.com/cmake/cmake/-/issues/16716 is ever resolved, we can use BIO_VERSION instead of
-# BIO_PROJECT_VERSION.
-#
-# BIO_PROJECT_VERSION is intended to be used within `project (... VERSION "${BIO_PROJECT_VERSION}")`.
-set (BIO_PROJECT_VERSION "${PACKAGE_VERSION}")
-if (BIO_RELEASE_CANDIDATE_VERSION VERSION_GREATER "0")
-    set (PACKAGE_VERSION "${PACKAGE_VERSION}-rc.${BIO_RELEASE_CANDIDATE_VERSION}")
-endif ()
-
-if (NOT BIO_PROJECT_VERSION VERSION_EQUAL PACKAGE_VERSION)
-    # Note: depending on how https://gitlab.kitware.com/cmake/cmake/-/issues/16716 is resolved (whether they use semver
-    # comparison semantics), (NOT "3.0.3" VERSION_GREATER_EQUAL "3.0.3-rc.1") might be the correct expression.
-    message (AUTHOR_WARNING "BIO_PROJECT_VERSION and BIO_VERSION mismatch, please report this issue and mention your cmake version.")
-endif ()
-
 # if the installed or the using project don't have CMAKE_SIZEOF_VOID_P set, ignore it:
 if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "")
     return()
