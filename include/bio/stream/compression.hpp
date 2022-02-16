joshuak94/b.i.o.

[misc] add 'secondary' extensions

include/bio/stream/compression.hpp
@@ -58,7 +58,19 @@ struct compression_traits
      * Note that this static member is not `const` and may be modified to allow
      * user-provided extensions.
      */
-    static inline std::vector<std::string> file_extensions = {"gz", "bgz", "bgzf"};
+    static inline std::vector<std::string> file_extensions;
+
+    /*!\brief Valid file extensions that have significance of their own.
+     *
+     * \details
+     *
+     * Note that this static member is not `const` and may be modified to allow
+     * user-provided extensions.
+     *
+     * These file extensions are not "additional" (e.g. the ".gz" in ".tar.gz")
+     * but still indicate the usage of a specific compression (e.g. ".tgz").
+     */
+    static inline std::vector<std::string> secondary_file_extensions;
 
     //!\brief The magic byte sequence to disambiguate the compression format.
     static constexpr std::string_view magic_header = "";
@@ -79,6 +91,9 @@ struct compression_traits<compression_format::bgzf> : compression_traits<compres
 
     //!\copydoc bio::compression_traits<compression_format::none>::file_extensions
     static inline std::vector<std::string> file_extensions = {"gz", "bgz", "bgzf"};
+
+    //!\copydoc bio::compression_traits<compression_format::none>::secondary_file_extensions
+    static inline std::vector<std::string> secondary_file_extensions = {"bcf", "bam"};
 
     //!\copydoc bio::compression_traits<compression_format::none>::magic_header
     static constexpr std::string_view magic_header{// GZip header
@@ -291,7 +306,7 @@ inline compression_format detect_format_from_magic_header(std::string_view const
 }
 
 //-------------------------------------------------------------------------------
-// detect_format_from_filename
+// detect_format_from_extension
 //-------------------------------------------------------------------------------
 
 /*!\brief Deduce bio::compression_format from a filename extension.
@@ -301,7 +316,7 @@ inline compression_format detect_format_from_magic_header(std::string_view const
  * Note that this function checks BGZF before GZ which means that it always selects BGZF for the extension ".gz".
  * This is desired, because many biological formats expect this.
  */
-inline compression_format detect_format_from_filename(std::filesystem::path const & path)
+inline compression_format detect_format_from_extension(std::filesystem::path const & path)
 {
     auto ext = path.extension().string();
 
@@ -323,6 +338,41 @@ inline compression_format detect_format_from_filename(std::filesystem::path cons
         if (ext == ext2)
             return compression_format::zstd;
     for (std::string_view const ext2 : compression_traits<compression_format::bgzf>::file_extensions)
+        if (ext == ext2)
+            return compression_format::bgzf;
+
+    return compression_format::none;
+}
+
+/*!\brief Deduce bio::compression_format from a filename extension.
+ * \ingroup stream
+ * \details
+ *
+ * Note that this function checks BGZF before GZ which means that it always selects BGZF for the extension ".gz".
+ * This is desired, because many biological formats expect this.
+ */
+inline compression_format detect_format_from_secondary_extension(std::filesystem::path const & path)
+{
+    auto ext = path.extension().string();
+
+    if (!ext.starts_with('.'))
+        return compression_format::none;
+    else
+        ext = ext.substr(1);
+
+    for (std::string_view const ext2 : compression_traits<compression_format::bgzf>::secondary_file_extensions)
+        if (ext == ext2)
+            return compression_format::bgzf;
+    for (std::string_view const ext2 : compression_traits<compression_format::gz>::secondary_file_extensions)
+        if (ext == ext2)
+            return compression_format::gz;
+    for (std::string_view const ext2 : compression_traits<compression_format::bz2>::secondary_file_extensions)
+        if (ext == ext2)
+            return compression_format::bz2;
+    for (std::string_view const ext2 : compression_traits<compression_format::zstd>::secondary_file_extensions)
+        if (ext == ext2)
+            return compression_format::zstd;
+    for (std::string_view const ext2 : compression_traits<compression_format::bgzf>::secondary_file_extensions)
         if (ext == ext2)
             return compression_format::bgzf;
 

include/bio/stream/transparent_ostream.hpp
@@ -122,6 +122,9 @@ struct transparent_ostream_options
 class transparent_ostream : public std::basic_ostream<char>
 {
 private:
+    //!\brief The base type.
+    using base_t = std::basic_ostream<char>;
+
     /*TODO
      * evaluate whether to use custom sized buffer on both streams
      * evaluate whether to use custom sized buffer strings passed in (what happens to stuff in old buffer?)
@@ -159,9 +162,17 @@ private:
         if (options_.compression == compression_format::detect)
         {
             if (filename_.empty()) // constructed from ostream → there is nothing to detect
+            {
                 options_.compression = compression_format::none;
+            }
             else
-                options_.compression = detail::detect_format_from_filename(filename_);
+            {
+                options_.compression = detail::detect_format_from_extension(filename_);
+                if (options_.compression != compression_format::none)
+                    truncated_filename_.replace_extension(); // remove compression extension
+                else
+                    options_.compression = detail::detect_format_from_secondary_extension(filename_);
+            }
         }
 
         // Thread handling
@@ -174,28 +185,23 @@ private:
                 --options_.threads; // bgzf spawns **additional** threads, but user sets total
         }
 
-        std::span<std::string> file_extensions{};
-        std::ostream *         sec = nullptr;
+        std::ostream * sec = nullptr;
         switch (options_.compression)
         {
             case compression_format::bgzf:
-                sec             = detail::make_ostream<compression_format::bgzf>(*primary_stream,
+                sec = detail::make_ostream<compression_format::bgzf>(*primary_stream,
                                                                      options_.threads,
                                                                      static_cast<size_t>(8ul),
                                                                      options_.compression_level);
-                file_extensions = compression_traits<compression_format::bgzf>::file_extensions;
                 break;
             case compression_format::gz:
                 sec = detail::make_ostream<compression_format::gz>(*primary_stream, options_.compression_level);
-                file_extensions = compression_traits<compression_format::gz>::file_extensions;
                 break;
             case compression_format::bz2:
                 sec = detail::make_ostream<compression_format::bz2>(*primary_stream, options_.compression_level);
-                file_extensions = compression_traits<compression_format::bz2>::file_extensions;
                 break;
             case compression_format::zstd:
                 sec = detail::make_ostream<compression_format::zstd>(*primary_stream, options_.compression_level);
-                file_extensions = compression_traits<compression_format::zstd>::file_extensions;
                 break;
             default:
                 break;
@@ -205,14 +211,6 @@ private:
             secondary_stream = stream_ptr_t{&*primary_stream, stream_deleter_noop};
         else
             secondary_stream = stream_ptr_t{sec, stream_deleter_default};
-
-        // truncate the filename in truncated_filename_ to show that compression has taken place
-        if (filename_.has_extension())
-        {
-            std::string extension = filename_.extension().string().substr(1);
-            if (std::ranges::find(file_extensions, extension) != std::ranges::end(file_extensions))
-                truncated_filename_.replace_extension();
-        }
     }
 
     //!\brief Initialise state of object.
@@ -234,10 +232,10 @@ public:
      */
     //!\brief Manually defined default constructor that behaves as expected.
     transparent_ostream() : std::basic_ostream<char>{} {}                  //!< Call default constructor of base.
-    transparent_ostream(transparent_ostream const &) = delete;             //!< Defaulted.
+    transparent_ostream(transparent_ostream const &)             = delete; //!< Defaulted.
     transparent_ostream & operator=(transparent_ostream const &) = delete; //!< Defaulted.
     // TODO double check that this works:
-    transparent_ostream & operator=(transparent_ostream &&) = default; //!< Defaulted.
+    transparent_ostream & operator=(transparent_ostream &&)      = default; //!< Defaulted.
 
     //!\brief Manually defined move constructor that behaves as expected.
     transparent_ostream(transparent_ostream && rhs)
@@ -320,6 +318,12 @@ public:
      * If this object was not created from a file, an empty path is returned.
      */
     std::filesystem::path const & truncated_filename() { return truncated_filename_; }
+
+    //!\brief Expose the base class's rdbuf which also accepts an argument.
+    using base_t::rdbuf;
+
+    //!\brief Expose the read buffer.
+    std::basic_streambuf<char> * rdbuf() const { return secondary_stream->rdbuf(); }
 };
 
 } // namespace bio

biocpp/biocpp-io

include/bio/stream/compression.hpp
@@ -58,7 +58,19 @@ struct compression_traits
      * Note that this static member is not `const` and may be modified to allow
      * user-provided extensions.
      */
-    static inline std::vector<std::string> file_extensions = {"gz", "bgz", "bgzf"};
+    static inline std::vector<std::string> file_extensions;
+
+    /*!\brief Valid file extensions that are not added to an existing extension but (usually) still imply compression.
+     *
+     * \details
+     *
+     * Note that this static member is not `const` and may be modified to allow
+     * user-provided extensions.
+     *
+     * These file extensions are not "additional" (e.g. the ".gz" in ".tar.gz")
+     * but still indicate the usage of a specific compression (e.g. ".tgz").
+     */
+    static inline std::vector<std::string> implied_file_extensions;
 
     //!\brief The magic byte sequence to disambiguate the compression format.
     static constexpr std::string_view magic_header = "";
@@ -79,6 +91,9 @@ struct compression_traits<compression_format::bgzf> : compression_traits<compres
 
     //!\copydoc bio::compression_traits<compression_format::none>::file_extensions
     static inline std::vector<std::string> file_extensions = {"gz", "bgz", "bgzf"};
+
+    //!\copydoc bio::compression_traits<compression_format::none>::implied_file_extensions
+    static inline std::vector<std::string> implied_file_extensions = {"bcf", "bam"};
 
     //!\copydoc bio::compression_traits<compression_format::none>::magic_header
     static constexpr std::string_view magic_header{// GZip header
@@ -155,7 +170,7 @@ struct compression_traits<compression_format::zstd> : compression_traits<compres
     //!\copydoc bio::compression_traits<compression_format::none>::magic_header
     static constexpr std::string_view magic_header{"\x28\xb5\x2f\xfd", 4};
 
-    // NOT YET SUPPORTED BY SEQAN
+    // NOT YET SUPPORTED BY BIO
 };
 
 } // namespace bio
@@ -291,7 +306,7 @@ inline compression_format detect_format_from_magic_header(std::string_view const
 }
 
 //-------------------------------------------------------------------------------
-// detect_format_from_filename
+// detect_format_from_extension
 //-------------------------------------------------------------------------------
 
 /*!\brief Deduce bio::compression_format from a filename extension.
@@ -301,7 +316,7 @@ inline compression_format detect_format_from_magic_header(std::string_view const
  * Note that this function checks BGZF before GZ which means that it always selects BGZF for the extension ".gz".
  * This is desired, because many biological formats expect this.
  */
-inline compression_format detect_format_from_filename(std::filesystem::path const & path)
+inline compression_format detect_format_from_extension(std::filesystem::path const & path)
 {
     auto ext = path.extension().string();
 
@@ -323,6 +338,41 @@ inline compression_format detect_format_from_filename(std::filesystem::path cons
         if (ext == ext2)
             return compression_format::zstd;
     for (std::string_view const ext2 : compression_traits<compression_format::bgzf>::file_extensions)
+        if (ext == ext2)
+            return compression_format::bgzf;
+
+    return compression_format::none;
+}
+
+/*!\brief Deduce bio::compression_format from a filename extension.
+ * \ingroup stream
+ * \details
+ *
+ * Note that this function checks BGZF before GZ which means that it always selects BGZF for the extension ".gz".
+ * This is desired, because many biological formats expect this.
+ */
+inline compression_format detect_format_from_implied_extension(std::filesystem::path const & path)
+{
+    auto ext = path.extension().string();
+
+    if (!ext.starts_with('.'))
+        return compression_format::none;
+    else
+        ext = ext.substr(1);
+
+    for (std::string_view const ext2 : compression_traits<compression_format::bgzf>::implied_file_extensions)
+        if (ext == ext2)
+            return compression_format::bgzf;
+    for (std::string_view const ext2 : compression_traits<compression_format::gz>::implied_file_extensions)
+        if (ext == ext2)
+            return compression_format::gz;
+    for (std::string_view const ext2 : compression_traits<compression_format::bz2>::implied_file_extensions)
+        if (ext == ext2)
+            return compression_format::bz2;
+    for (std::string_view const ext2 : compression_traits<compression_format::zstd>::implied_file_extensions)
+        if (ext == ext2)
+            return compression_format::zstd;
+    for (std::string_view const ext2 : compression_traits<compression_format::bgzf>::implied_file_extensions)
         if (ext == ext2)
             return compression_format::bgzf;
 

include/bio/stream/transparent_ostream.hpp
@@ -122,6 +122,9 @@ struct transparent_ostream_options
 class transparent_ostream : public std::basic_ostream<char>
 {
 private:
+    //!\brief The base type.
+    using base_t = std::basic_ostream<char>;
+
     /*TODO
      * evaluate whether to use custom sized buffer on both streams
      * evaluate whether to use custom sized buffer strings passed in (what happens to stuff in old buffer?)
@@ -159,9 +162,17 @@ private:
         if (options_.compression == compression_format::detect)
         {
             if (filename_.empty()) // constructed from ostream → there is nothing to detect
+            {
                 options_.compression = compression_format::none;
+            }
             else
-                options_.compression = detail::detect_format_from_filename(filename_);
+            {
+                options_.compression = detail::detect_format_from_extension(filename_);
+                if (options_.compression != compression_format::none)
+                    truncated_filename_.replace_extension(); // remove compression extension
+                else
+                    options_.compression = detail::detect_format_from_implied_extension(filename_);
+            }
         }
 
         // Thread handling
@@ -174,28 +185,23 @@ private:
                 --options_.threads; // bgzf spawns **additional** threads, but user sets total
         }
 
-        std::span<std::string> file_extensions{};
-        std::ostream *         sec = nullptr;
+        std::ostream * sec = nullptr;
         switch (options_.compression)
         {
             case compression_format::bgzf:
-                sec             = detail::make_ostream<compression_format::bgzf>(*primary_stream,
+                sec = detail::make_ostream<compression_format::bgzf>(*primary_stream,
                                                                      options_.threads,
                                                                      static_cast<size_t>(8ul),
                                                                      options_.compression_level);
-                file_extensions = compression_traits<compression_format::bgzf>::file_extensions;
                 break;
             case compression_format::gz:
                 sec = detail::make_ostream<compression_format::gz>(*primary_stream, options_.compression_level);
-                file_extensions = compression_traits<compression_format::gz>::file_extensions;
                 break;
             case compression_format::bz2:
                 sec = detail::make_ostream<compression_format::bz2>(*primary_stream, options_.compression_level);
-                file_extensions = compression_traits<compression_format::bz2>::file_extensions;
                 break;
             case compression_format::zstd:
                 sec = detail::make_ostream<compression_format::zstd>(*primary_stream, options_.compression_level);
-                file_extensions = compression_traits<compression_format::zstd>::file_extensions;
                 break;
             default:
                 break;
@@ -205,14 +211,6 @@ private:
             secondary_stream = stream_ptr_t{&*primary_stream, stream_deleter_noop};
         else
             secondary_stream = stream_ptr_t{sec, stream_deleter_default};
-
-        // truncate the filename in truncated_filename_ to show that compression has taken place
-        if (filename_.has_extension())
-        {
-            std::string extension = filename_.extension().string().substr(1);
-            if (std::ranges::find(file_extensions, extension) != std::ranges::end(file_extensions))
-                truncated_filename_.replace_extension();
-        }
     }
 
     //!\brief Initialise state of object.
@@ -234,10 +232,10 @@ public:
      */
     //!\brief Manually defined default constructor that behaves as expected.
     transparent_ostream() : std::basic_ostream<char>{} {}                  //!< Call default constructor of base.
-    transparent_ostream(transparent_ostream const &) = delete;             //!< Defaulted.
+    transparent_ostream(transparent_ostream const &)             = delete; //!< Defaulted.
     transparent_ostream & operator=(transparent_ostream const &) = delete; //!< Defaulted.
     // TODO double check that this works:
-    transparent_ostream & operator=(transparent_ostream &&) = default; //!< Defaulted.
+    transparent_ostream & operator=(transparent_ostream &&)      = default; //!< Defaulted.
 
     //!\brief Manually defined move constructor that behaves as expected.
     transparent_ostream(transparent_ostream && rhs)
@@ -320,6 +318,12 @@ public:
      * If this object was not created from a file, an empty path is returned.
      */
     std::filesystem::path const & truncated_filename() { return truncated_filename_; }
+
+    //!\brief Expose the base class's rdbuf which also accepts an argument.
+    using base_t::rdbuf;
+
+    //!\brief Expose the read buffer.
+    std::basic_streambuf<char> * rdbuf() const { return secondary_stream->rdbuf(); }
 };
 
 } // namespace bio