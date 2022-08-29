4bedfe0717b8057b774b2f008544e63d22b18b68
Hannes Hauswedell <hannes.hauswedell@decode.is>
2022-08-29 16:28:29 +0000

    [infra] remove seqan3 dependency and submodule

--- build_system/biocpp_io-config.cmake
+++ build_system/biocpp_io-config.cmake
@@ -172,20 +172,6 @@ else ()
 endif ()
 
 # ----------------------------------------------------------------------------
-# Require SeqAn3
-# ----------------------------------------------------------------------------
-
-find_package (SeqAn3 REQUIRED QUIET
-              HINTS ${CMAKE_CURRENT_LIST_DIR}/../submodules/seqan3/build_system)
-
-if (SEQAN3_FOUND)
-    bio_config_print ("Required dependency:        SeqAn3 found (${SEQAN3_VERSION}).")
-    set (BIOCPP_IO_LIBRARIES ${BIOCPP_IO_LIBRARIES} seqan3_seqan3)
-else ()
-    bio_config_print ("The SeqAn3 library is required, but wasn't found. Get it from https://github.com/seqan/seqan3")
-endif ()
-
-# ----------------------------------------------------------------------------
 # ZLIB dependency
 # ----------------------------------------------------------------------------
 
